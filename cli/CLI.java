5b2531e580835649c02be4a8ba0b17c1beefcc3b
Raphael Käser <rooftop_nervier_0e@icloud.com>
2024-12-22 21:23:58 +0100

    add a basic input function

--- cli/CLI.java
+++ cli/CLI.java
@@ -1,11 +1,19 @@
 package cli;
 
+import java.util.Scanner;
+
 public class CLI {
     private Level currentLevel;
 
     enum Level {
         MAIN,
         PLAYER
+    }
+
+    public String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return input;
     }
 
 }
