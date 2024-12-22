9a00278de7c25bfc2bff3564d7ec885a83c9b01d
Raphael Käser <rooftop_nervier_0e@icloud.com>
2024-12-22 21:24:16 +0100

    add basic connect function

--- database/DB.java
+++ database/DB.java
@@ -3,15 +3,23 @@ package database;
 import java.sql.*;
 
 public class DB {
+    private String password;
+    private String username;
+    private String url;
 
-    private String path;
-
-    public DB(String path) {
-        this.path = path;
+    public DB(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
     }
 
     public void connect() {
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
 
+        } catch (SQLException e) {
+            // TODO add error log
+        }
     }
 
 }
