54416358bee1539cd7a5cf2e8a3a13f4bc39a5e5
Raphael Käser <rooftop_nervier_0e@icloud.com>
2024-12-25 21:08:17 +0100

    test user creation method

--- tests/DBTest.java
+++ tests/DBTest.java
@@ -1,8 +1,55 @@
 package tests;
 
+import database.DB;
+import config.Config;
+import log.Log;
+import java.util.Arrays;
+import java.util.List;
+import user.User;
+
 public class DBTest {
+    private static Log logger = new Log();
+    private static Config config = new Config("./config/config.json");
+    private static DB db = new DB(config.getDbAddress(), config.getDbUser(), config.getDbPassword());
+
     public static void runTests() {
         // Add test logic here
-        System.out.println("DBTest passed");
+        db.connect();
+        try {
+            DBTest.testIfUserTablesExists();
+            DBTest.testUser();
+        } catch (AssertionError e) {
+            logger.error("Failed to create all user tables");
+        }
+        logger.info("DB connection successful");
+
+    }
+
+    private static void testIfUserTablesExists() throws AssertionError {
+        db.createNewUserTables();
+        String[] tables = db.getAllTables(config.getDbName());
+        List<String> tableList = Arrays.asList(tables);
+        assert tableList.contains("users");
+        assert tableList.contains("banks");
+        assert tableList.contains("games");
+
+    }
+
+    private static void testUser() {
+        User user = new User("test", "raphael");
+        user.hashPassword();
+        assert user.password != "test";
+
+        db.createUser(user);
+        User foundUser = db.getUserByName(user.name);
+        logger.info("Found user id: " + foundUser.id);
+        assert foundUser.name.equals(user.name);
+        assert foundUser.password.equals(user.password);
+
+    }
+
+    private static void testBank() {
+        // TODO Add test logic here
+
     }
 }
